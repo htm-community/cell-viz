4b417dc1322327fa73c074d952760ca29c1e0b44
rhyolight <rhyolight@gmail.com>
2018-04-09 11:18:38 -0700

    1.4.6: hovers and lines

--- src/SdrDrawing.js
+++ src/SdrDrawing.js
@@ -1,6 +1,6 @@
 /* From http://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage */
 function getGreenToRed(percent){
-    var r, g;
+    let r, g;
     percent = 100 - percent;
     r = percent < 50 ? 255 : Math.floor(255-(percent*2-100)*255/100);
     g = percent > 50 ? 255 : Math.floor((percent*2)*255/100);
@@ -12,6 +12,13 @@ function rgbToHex(r, g, b) {
     return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
+function datumIsOn(d) {
+    return d !== null && d > 0
+}
+function datumIsConnected(d, threshold) {
+    return d !== null && threshold !== null && d > threshold
+}
+
 let defaultOpts = {
     width: 400,
     height: 400,
@@ -19,12 +26,14 @@ let defaultOpts = {
     gradientFill: false,
     onColor: 'skyblue',
     offColor: 'white',
-    connectionColor: 'royalblue'
+    connectionColor: 'royalblue',
+    lineColor: 'teal',
 }
 
 function SdrDrawing(permanences, element) {
     this.permanences = permanences
     this.el = element
+    this.$drawing = undefined
 }
 
 SdrDrawing.prototype._snapDrawOptionsToBox = function(opts) {
@@ -39,18 +48,65 @@ SdrDrawing.prototype._snapDrawOptionsToBox = function(opts) {
     return opts
 }
 
+SdrDrawing.prototype.onCell = function(eventName, fn) {
+    this.$drawing.selectAll('rect.bit').on(eventName, fn)
+    return this
+}
+
+SdrDrawing.prototype.onConnection = function(eventName, fn) {
+    this.$drawing.selectAll('circle.connection').on(eventName, fn)
+    return this
+}
+
+SdrDrawing.prototype.drawLinesTo = function(coords) {
+    let data = this.$drawing.selectAll('rect.bit').data()
+    let opts = this.drawOptions
+
+    function renderLines(ls) {
+        ls.attr('class', 'line')
+            .attr('visibility', (d, i) => {
+                if (datumIsOn(d)) return 'visible'
+                else return 'hidden'
+            })
+            .attr('stroke', opts.lineColor)
+            .attr('stroke-width', 1.0)
+            .attr('x1', function(d, i) {
+                let offset = i % opts.rowLength;
+                return offset * opts.cellSize + opts.cellSize / 2;
+            })
+            .attr('y1', function(d, i) {
+                let offset = Math.floor(i / opts.rowLength);
+                return offset * opts.cellSize + opts.cellSize / 2;
+            })
+            .attr('x2', coords[0])
+            .attr('y2', coords[1])
+    }
+
+    // Update
+    let lines = this.$drawing.selectAll('line.line').data(data)
+    renderLines(lines)
+
+    // Enter
+    let newLines = lines.enter().append('line')
+    renderLines(newLines)
+
+    // Exit
+    lines.exit().remove()
+
+    return this
+}
+
 SdrDrawing.prototype.draw = function(options) {
     let opts = this._snapDrawOptionsToBox(Object.assign(defaultOpts, options))
     let threshold = opts.threshold
     let perms = this.permanences
-    let svg = d3.select('#' + this.el)
+    this.$drawing = d3.select('#' + this.el)
         .attr('width', opts.width)
         .attr('height', opts.height)
 
-    svg.html('')
-
     function renderCell(r, c) {
-        r.attr('fill', (d) => {
+        r.attr('class', 'bit')
+            .attr('fill', (d) => {
                 if (d === null) return opts.offColor
                 if (d > 0) {
                     if (opts.gradientFill) return '#' + getGreenToRed(d * 100)
@@ -71,43 +127,59 @@ SdrDrawing.prototype.draw = function(options) {
             })
             .attr('width', opts.cellSize)
             .attr('height', opts.cellSize)
-        c.attr('fill', (d, i) => {
-                // If no data, means it is empty or zero bit, no circles.
-                if (d === null) return 'none'
-                // If there is a threshold defined, we'll assume the perms are
-                // not binary, but are permanences values, and will render
-                // circles.
-                if (threshold !== undefined) {
-                    if (d > threshold) return opts.connectionColor
-                }
-                return 'none'
-            })
-            .attr('fill-opacity', 1)
-            .attr('cx', function(d, i) {
-                let offset = i % opts.rowLength;
-                return offset * opts.cellSize + (opts.cellSize / 2);
-            })
-            .attr('cy', function(d, i) {
-                let offset = Math.floor(i / opts.rowLength);
-                return offset * opts.cellSize + (opts.cellSize / 2);
-            })
-            .attr('r', opts.cellSize / 4)
+
+        if (c) {
+            c.attr('class', 'connection')
+                .attr('fill', opts.connectionColor)
+                .attr('cx', function(d) {
+                    let i = d.index
+                    let offset = i % opts.rowLength;
+                    return offset * opts.cellSize + (opts.cellSize / 2);
+                })
+                .attr('cy', function(d) {
+                    let i = d.index
+                    let offset = Math.floor(i / opts.rowLength);
+                    return offset * opts.cellSize + (opts.cellSize / 2);
+                })
+                .attr('r', opts.cellSize / 4)
+        }
     }
 
     // Update
-    let rects = svg.selectAll('rect').data(perms)
-    let circs = svg.selectAll('circles').data(perms)
+    let rects = this.$drawing.selectAll('rect.bit').data(perms)
+    let circs
+    // Only create circles if there is a threshold defined for connections.
+    if (threshold !== undefined) {
+        let permObjs = perms.map((p, i) => {
+            return {
+                index: i,
+                permanence: p
+            }
+        }).filter((p) => {
+            return p.permanence !== null && p.permanence > threshold
+        })
+        circs = this.$drawing.selectAll('circle.connection').data(permObjs)
+    }
     renderCell(rects, circs)
 
     // Enter
     let newRects = rects.enter().append('rect')
-    let newCircs = circs.enter().append('circle')
+    let newCircs
+    if (threshold !== undefined) {
+        newCircs = circs.enter().append('circle')
+    }
     renderCell(newRects, newCircs)
 
     // Exit
     rects.exit().remove()
-    circs.exit().remove()
+    if (threshold !== undefined) {
+        circs.exit().remove()
+    }
 
+    // Stash the draw options we used
+    this.drawOptions = opts
+
+    return this
 }
 
 module.exports = SdrDrawing
